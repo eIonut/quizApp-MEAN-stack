bc8a18948214ed20d6992f88ab55bdbe0edc30e1
eIonut <enacheionut2399@gmail.com>
2022-09-22 09:41:16 +0300

    added processenv

--- src/app-back/app.js
+++ src/app-back/app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use('/api/v1/quizzes', quizzes);
 app.use(errorHandlerMiddleware);
 
-const port =  process.env.PORT;
+const port =  process.env.PORT || 3000;
 
 const start = async () => {
     try {
