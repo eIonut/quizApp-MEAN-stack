3c2673468f15bc66b9fc85ba003b9e6d88e2a7b6
eIonut <enacheionut2399@gmail.com>
2022-09-22 10:30:13 +0300

    mongo

--- src/app-back/app.js
+++ src/app-back/app.js
@@ -6,6 +6,15 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 
 require('dotenv').config();
 
+var MongoClient = require('mongodb').MongoClient;
+
+var username = 'ionut1234';
+var password = 'ionut1234';
+var hosts = 'iad2-c16-0.mongo.objectrocket.com:52316,iad2-c16-2.mongo.objectrocket.com:52316,iad2-c16-1.mongo.objectrocket.com:52316';
+var database = 'QUIZ-APP';
+var options = '?replicaSet=6d91059048ab4f49a42ddf0ca33cc3fe';
+var connectionString = 'mongodb://' + username + ':' + password + '@' + hosts + '/' + database + options;
+
 app.use(express.static('./public'));
 app.use(express.json());
 
@@ -14,16 +23,28 @@ app.use(errorHandlerMiddleware);
 
 const port =  process.env.PORT || 3000;
 
-const start = async () => {
-    try {
-        await connectDB(process.env.MONGO_URI);
-        app.listen(port, console.log(`Server is listening on port ${port}`));
-    } catch (error) {
-        console.log(error)
-    }
-}
+MongoClient.connect(connectionString, function(err, db) {
+  if (db) {
+    db.close();
+  }
+  if (err) {
+    console.log('Error: ', err);
+  } else {
+    console.log('Connected!');
+    process.exit();
+  }
+});
 
-start();
+// const start = async () => {
+//     try {
+//         await connectDB(process.env.MONGO_URI);
+//         app.listen(port, console.log(`Server is listening on port ${port}`));
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+//
+// start();
 
 
 
